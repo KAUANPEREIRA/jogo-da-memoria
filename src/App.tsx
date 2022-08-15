bbde856b5f5033785fd5365825f32e4e27c4c2b9
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-08-15 09:58:12 -0300

    Criando ambiente
    Criando ambiente do projeto jogo da memória

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import * as C from './App.styles'
+import LogoImg from './assets/devmemory_logo.png'
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Um dia vai chegar o dia</h1>
-    </div>
-  );
+const App = ()=>{
+  return(
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={LogoImg} width="200px"alt=''/>
+         </C.LogoLink>
+
+         <C.InfoArea>
+
+         </C.InfoArea>
+         <button>Reiniciar</button>
+
+      </C.Info>
+      <C.GridArea>
+
+      </C.GridArea>
+    </C.Container>
+  )
+
 }
 
-export default App;
+export default App
